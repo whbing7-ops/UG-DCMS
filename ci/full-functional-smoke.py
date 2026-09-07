97497b6a5bbb60c863eeb7efd638375b3b91f8ba
whbing7-ops <whbing7@gmail.com>
2026-09-07 23:59:44 +0800

    Fix randomized admin password policy branch

--- ci/full-functional-smoke.py
+++ ci/full-functional-smoke.py
@@ -23,7 +23,7 @@ PASSWORDS = {
     "cm": ("UG.ConfigMgr!" + STAMP, "UG.ConfigMgr2!" + STAMP),
 }
 if len(sys.argv) > 2:
-    PASSWORDS["admin"] = (sys.argv[2], "UG.Admin!" + STAMP)
+    PASSWORDS["admin"] = (sys.argv[2], "Changed!Root" + STAMP)
 
 
 class Client:
